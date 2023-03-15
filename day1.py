e8d1788fe4b76d628f9867f73f557488241a1310
sangamk04 <99814514+sangamk04@users.noreply.github.com>
2023-03-15 13:30:12 +0530

    ok

--- day1.py
+++ day1.py
@@ -4,9 +4,11 @@
 # b=4;
 # print (a+b)
 
-num1=int(input("enter first num :"))
-num2=int(input("enter second num :"))
+# num1=int(input("enter first num :"))
+# num2=int(input("enter second num :"))
 
-sum =num1+num2
+# sum =num1+num2
 
-print("sum:", sum)
+# print("sum:", sum)
+
+
